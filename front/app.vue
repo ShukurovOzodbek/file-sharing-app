33af440181e0ec3cd5d0ee91c8be7785c7aa7287
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-12-29 13:48:26 +0500

    basic setup in frontend

--- front/app.vue
+++ front/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <nuxt-layout />
 </template>
+
+<script>
+</script>
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  srcDir: './src',
+  css: ['~/public/index.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
