781838fad18cf6c999edb0789236539bf1409f4f
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-12-29 12:48:44 +0500

    basic setup

--- back/app.ts
+++ back/app.ts
@@ -1,16 +1,12 @@
 import createError from 'http-errors';
 import express from 'express';
-import http from 'http';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import router from './src/routes';
+import http from 'http';
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,10 +14,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', router);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
+
 
 // error handler
 app.use(function (err: any, req: any, res: any, next: any) {
@@ -34,12 +33,12 @@ app.use(function (err: any, req: any, res: any, next: any) {
     res.render('error');
 });
 
-const server = http.createServer((res, req) => {
-    console.log('server created');
-});
+
+const server = http.createServer(app);
+
 
 server.listen(3030, () => {
-    console.log('Server runned on port http://localhost:3030');
+    console.log('Server is running on port http://localhost:3030');
 })
 
-module.exports = app;
+export default app;
