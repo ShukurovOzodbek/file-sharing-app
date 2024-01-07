b63407b6ae39d528916562a7d5b19bc28318777a
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-01-07 22:48:41 +0500

    auth middleware

--- front/src/middleware/auth.ts
+++ front/src/middleware/auth.ts
@@ -1,3 +1,24 @@
-export default defineNuxtRouteMiddleware((to) => {
-    console.log(to);
+import axios from "axios";
+import {parseJwt} from "~/utils/auth";
+
+export default defineNuxtRouteMiddleware(async () => {
+    const token: any = useCookie('token');
+    const url = import.meta.env.VITE_API_URL;
+    
+    if (!token.value) {
+        return navigateTo('/login');
+    }
+    
+    const parsed = parseJwt(token.value);
+    
+    if (!parsed?.userId) {
+        return navigateTo('/login');
+    }
+    
+    try {
+        await axios.get(`${url}/users/${parsed.userId}`)
+    } catch (e) {
+        return navigateTo('/login');
+    }
+    
 });
--- front/src/utils/auth.ts
+++ front/src/utils/auth.ts
@@ -9,3 +9,11 @@ export const signup = (password: string, email: string) => {
     const url = import.meta.env.VITE_API_URL;
     return axios.post(`${url}/users`, {email, password});
 }
+
+export function parseJwt(token: string) {
+    try {
+        return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+        return null;
+    }
+}
